d80d26767c392d3a43be5fe54fed401f133bdba4
Sharayu <harshutale21@gmail.com>
2025-06-05 17:40:15 +0530

    all files updated

---
+++ react-state/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from "./Counter.jsx"
+import LikeButton from "./LikeButton.jsx"
+
+function App() {
+  return (
+  <>
+  <h4>States in react</h4>
+  <LikeButton/>
+    
+  </>
+  )
+  
+}
+
+export default App
